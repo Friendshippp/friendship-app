88403ad95a2988850e751361c745d903279940ab
Asaf Gerchak <asaf.gerchak@gmail.com>
2019-12-03 16:51:16 -0500

    Important updates :fire::imp::gun::bomb:

--- src/App.js
+++ src/App.js
@@ -8,8 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ABANDON ALL HOPE
         </p>
+        <p>THERE IS ONLY MEANINGLESS MISERY</p>
+        <p>GIVE UP</p>
+        <p>NO REST</p>
+        <p>NO FREEDOM</p>
+        <p>DEATH IS NO ESCAPE</p>
+        <p>FEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEARFEAR FEAR FEAR FEAR FEAR</p>
         <a
           className="App-link"
           href="https://reactjs.org"
